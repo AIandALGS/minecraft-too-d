f9b59f6073b3f8b71e0466928590b2f30a5c82d5
AIandALGS <gallonzhou04@gmail.com>
2023-06-22 01:47:56 +1200

    Added a music manager to the game

--- manager/event_manager.py
+++ manager/event_manager.py
@@ -1,10 +1,12 @@
 import pygame
 
+from manager.music_manager import MusicManager
+
 
 class EventManager:
 
     def __init__(self) -> None:
-        ...
+        self.__music_manager = MusicManager
 
     def poll_events(self) -> bool:
         game_running = True
@@ -12,5 +14,8 @@ class EventManager:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+
+        if not (pygame.mixer.music.get_busy()):
+            self.__music_manager.play_music()
 
         return game_running
--- manager/music_manager.py
+++ manager/music_manager.py
@@ -1,4 +1,28 @@
+from random import randint
+from pygame import mixer
+
+from os import listdir
+
+
 class MusicManager:
 
     def __init__(self):
-        self.path = "sound_fx"
+        self.music_path = "sound_fx"
+
+        self.music_list = listdir(self.music_path)
+        self.music_poll = len(self.music_list) - 1
+
+    def play_music(self):
+        music_rand = randint(0, self.music_poll)
+
+        music_live = self.music_path + "/" + self.music_list[music_rand]
+
+        mixer.music.load(music_live)
+        mixer.music.play()
+
+        print("Now playing: " + ''.join(self.music_list[music_rand]))
+
+
+music_manager = MusicManager()
+
+music_manager.play_music()
